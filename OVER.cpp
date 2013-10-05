e7c467a519333378463e0a666a62fd42ba2f260f
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2013-10-05 15:37:21 +0530

    adding solution for OVER in C++. Time=0.00, Memory=2688

---
+++ OVER.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+unsigned long long two[50],four[50],six[50];
+unsigned long long foo(int i, int j){
+	if(i==2){
+		if(two[j-1]==-1)
+		two[j-1]=foo(2,j-1) + foo(4,j-1);
+		return two[j-1];
+	}
+	if(i==4){
+		if(four[j-1]==-1)
+		four[j-1]=foo(4,j-1) + foo(2,j-1) + foo(6,j-1);
+		return four[j-1];
+	}
+	if(i==6){
+		if(six[j-1]==-1)
+		six[j-1]=foo(6,j-1) + foo(4,j-1);
+		return six[j-1];
+	}
+}
+
+int main(){
+	int t;
+	cin>>t;
+	for(int i=0;i<50;i++){
+		two[i]=four[i]=six[i]=-1;
+	}
+	two[0]=four[0]=six[0]=1;
+	foo(2,50);foo(4,50);foo(6,50);
+	while(t--){
+		int n;
+		cin>>n;
+		cout<<two[n-1]+four[n-1]+six[n-1]<<endl;
+	}
+	return 0;
+}
+		
+			
+		
